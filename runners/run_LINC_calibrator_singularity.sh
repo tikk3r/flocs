220b94b6b70b246940ff4e3d0d277755cf5d9036
Frits Sweijen <tikk3r@users.noreply.github.com>
2022-10-19 12:39:19 +0200

    Fix script path and execution issues

--- runners/run_LINC_calibrator_singularity.sh
+++ runners/run_LINC_calibrator_singularity.sh
@@ -1,17 +1,18 @@
 # Update these variables to reflect your environment.
-## Location of LINC. This must be a user-writable location for this wrapper script.
-## If it does not exist, this script will attempt to clone the LINC repository to the given path.
-LINC_DATA_ROOT=/path/to/LINC
-
-DATADIR=/path/to/data/
 TMPDIR=$PWD
-
-# Add any directory that should be accessible during the run here as a comma-separated list.
-BINDPATHS=/data1,/data2
-
 ## WORKDIR is where all the other directories will be stored.
 WORKDIR=$(mktemp -d -p "$TMPDIR")
 echo $WORKDIR
+## Location of LINC. This must be a user-writable location for this wrapper script.
+## If it does not exist, this script will attempt to clone the LINC repository to the given path.
+LINC_DATA_ROOT=$WORKDIR/LINC
+#LINC_DATA_ROOT=/data2/sweijen/Quasar_Anniek/LINC_calibrator/LINC
+
+DATADIR=/path/to/data
+
+# Add any directory that should be accessible during the run here as a comma-separated list.
+BINDPATHS=/data1,/data2,$PWD
+
 ## Final results will be copied here.
 RESULTSDIR=$WORKDIR/results_LINC_calibrator/
 ## Logs of the various steps will be put here.
@@ -20,7 +21,8 @@ LOGSDIR=$WORKDIR/logs_LINC_calibrator/
 ## The trailing slash is important here.
 TMPDIR=$WORKDIR/tmpdir_LINC_calibrator/
 
-SIMG=/path/to/container.sif
+#SIMG=/net/lofar1/data1/sweijen/software/LOFAR/singularity/lofar_sksp_v4.0.0_cascadelake_cascadelake_avx512_mkl_cuda_ddf.sif
+SIMG=/path/to/singularity/container.sif
 
 # Update these variables to tune performance.
 ## Limit the number of DP3 processes running simultaneously to this amount.
@@ -39,11 +41,15 @@ if [ ! -d $LINC_DATA_ROOT ]; then
     echo $LINC_DATA_ROOT does not exist and will be created. Cloning LINC...
     mkdir -p $LINC_DATA_ROOT
     git clone https://git.astron.nl/RD/LINC.git $LINC_DATA_ROOT
+    cd $LINC_DATA_ROOT && git checkout d4741f0
+    cd $WORKDIR
 fi
 # If the directory exists, check if it is empty.
 if [ -d $LINC_DATA_ROOT ] && [ ! -d $LINC_DATA_ROOT/steps ]; then
     echo $LINC_DATA_ROOT exists, but is empty. Cloning LINC...
     git clone https://git.astron.nl/RD/LINC.git $LINC_DATA_ROOT
+    cd $LINC_DATA_ROOT && git checkout d4741f0
+    cd $WORKDIR
 fi
 # If the directory is not empty, check if it contains LINC
 if [ -d $LINC_DATA_ROOT ] && [ ! -d $LINC_DATA_ROOT/steps ]; then
@@ -52,16 +58,24 @@ if [ -d $LINC_DATA_ROOT ] && [ ! -d $LINC_DATA_ROOT/steps ]; then
 elif [ -d $LINC_DATA_ROOT ] && [ -d $LINC_DATA_ROOT/steps ]; then
     echo $LINC_DATA_ROOT exists and seems to contain LINC. Continueing...
 fi
-export PYTHONPATH=$LINC_DATA_ROOT/scripts
 
 # Prepare workflow files.
 echo Overriding workflow core requirements with user settings.
-sed -i "s/coresMin: [0-9]*/coresMin: $CORES_CALIBCAL/" $LINC_DATA_ROOT/steps/ddecal.cwl
-sed -i "s/coresMin: [0-9]*/coresMin: $CORES_PREDICT/" $LINC_DATA_ROOT/steps/predict.cwl
+#sed -i "s/coresMin: [0-9]*/coresMin: $CORES_CALIBCAL/" $LINC_DATA_ROOT/steps/ddecal.cwl
+#sed -i "s/coresMin: [0-9]*/coresMin: $CORES_PREDICT/" $LINC_DATA_ROOT/steps/predict.cwl
+echo Making sure all scripts are executable
+chmod 755 $LINC_DATA_ROOT/scripts/*.py
+echo Making sure all shebang lines use /usr/bin/env python instead of /usr/bin/env
+for f in $LINC_DATA_ROOT/scripts/*.py; do
+    sed -i "s?\#\!/usr/bin/python?\#\!/usr/bin/env python?" $f
+done
 
 # Pass along necessary variables to the container.
 export APPTAINERENV_LINC_DATA_ROOT=$LINC_DATA_ROOT
-export APPTAINERENV_PYTHONPATH=$PYTHONPATH
+export APPTAINERENV_RESULTSDIR=$WORKDIR/results_LINC_calibrator/
+export APPTAINERENV_LOGSDIR=$WORKDIR/logs_LINC_calibrator/
+export APPTAINERENV_TMPDIR=$WORKDIR/tmpdir_LINC_calibrator/
+export APPTAINERENV_PREPEND_PATH=$LINC_DATA_ROOT/scripts
 
 mkdir -p $RESULTSDIR
 mkdir -p $LOGSDIR
@@ -72,5 +86,8 @@ wget https://raw.githubusercontent.com/tikk3r/lofar-grid-hpccloud/fedora-py3/run
 singularity exec -B $PWD,$BINDPATHS $SIMG python create_ms_list.py $DATADIR
 
 echo LINC starting
-time singularity exec -B $PWD,$BINDPATHS $SIMG cwltool --parallel --preserve-entire-environment --no-container --tmpdir-prefix=$TMPDIR --outdir=$RESULTSDIR --log-dir=$LOGSDIR $LINC_DATA_ROOT/workflows/HBA_calibrator.cwl mslist.json
+# Singularity doesn't seem to pass this along through APPTAINERENV_PREPEND, so create another little wrapper here to ensure it gets added.
+echo export PYTHONPATH=\$LINC_DATA_ROOT/scripts:\$PYTHONPATH > tmprunner.sh
+echo cwltool --parallel --preserve-entire-environment --no-container --tmpdir-prefix=$TMPDIR --outdir=$RESULTSDIR --log-dir=$LOGSDIR $LINC_DATA_ROOT/workflows/HBA_calibrator.cwl mslist.json >> tmprunner.sh
+time singularity exec -B $PWD,$BINDPATHS $SIMG bash tmprunner.sh
 echo LINC ended
