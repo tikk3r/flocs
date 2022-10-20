2b3b799b53af1626e61a4134533a1290ef7fe41c
Frits Sweijen <tikk3r@users.noreply.github.com>
2022-10-20 14:06:17 +0200

    Update mslist creator to include calibrator output

--- runners/create_ms_list.py
+++ runners/create_ms_list.py
@@ -1,20 +1,41 @@
+import argparse
 import glob
 import os
 import sys
 
-print('Searching ' + os.path.abspath(sys.argv[1]).rstrip('/') + '/*.MS')
-files = sorted(glob.glob(os.path.abspath(sys.argv[1]).rstrip('/') + '/*.MS'))
-#files = glob.glob(os.path.abspath(sys.argv[1]).rstrip('/') + '/*.MS')
-print(f'Found {len(files)} files')
+def make_input_json(mspath, calsols=''):
+    ''' Creates the basic input JSON file for the LINC calibrator and target workflows.
 
-with open('mslist.json', 'w') as out:
-    out.write('{\n')
-    out.write('  "msin": [\n')
-    for ms in files[:-1]:
-        out.write(f'   {{"class": "Directory", "path":"{ms}"}},\n')
-    else:
-        ms = files[-1]
-        out.write(f'   {{"class": "Directory", "path":"{ms}"}}\n')
-    out.write('  ],\n')
+    Args:
+        mspath (str): path where the input MS are stored.
+        calsols (str): path where cal_solutions.h5 from the calibrator run is stored.
+    Returns:
+        None
+    '''
+    print('Searching ' + os.path.abspath(mspath).rstrip('/') + '/*.MS')
+    files = sorted(glob.glob(os.path.abspath(mspath).rstrip('/') + '/*.MS'))
+    print(f'Found {len(files)} files')
+
+    with open('mslist.json', 'w') as out:
+        out.write('{\n')
+        out.write('  "msin": [\n')
+        for ms in files[:-1]:
+            out.write(f'   {{"class": "Directory", "path":"{ms}"}},\n')
+        else:
+            ms = files[-1]
+            out.write(f'   {{"class": "Directory", "path":"{ms}"}}\n')
+        out.write('  ],\n')
+    if calsols:
+            calsolpath = os.path.abspath(calsols)
+            out.write(f' "cal_solutions": {{"class": "File", "path":"{calsolpath}"}}\n')
     out.write('}\n')
-print('Input JSON written to mslist.json')
+    print('Input JSON written to mslist.json')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Generate an input file for LINC containing measurement sets and, optionally, the calibrator solutions.')
+    parser.add_argument('mspath', type=str, help='Path where input measurement sets are located.')
+    parser.add_argument('--calsols', type=str, default='', help='Path to the final LINC calibrator solution file (usually cal_solutions.h5).')
+
+    args = parser.parse_args()
+
+    make_input_json(args['mspath'], args['calsols'])
