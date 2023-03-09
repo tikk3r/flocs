2f1ab4cd770704feff52dee453e89c134c8cf5f3
Frits Sweijen <tikk3r@users.noreply.github.com>
2023-03-09 14:38:01 +0100

    Add runner for CWL version of lofar-vlbi

--- runners/create_ms_list.py
+++ runners/create_ms_list.py
@@ -1,9 +1,9 @@
 import argparse
 import glob
 import os
-import sys
+import sys 
 
-def make_input_json(mspath, calsols=''):
+def make_input_json(mspath, calsols='', targetsols='', path_facetselfcal_config='', path_lofar_helpers='', path_selfcal=''):
     ''' Creates the basic input JSON file for the LINC calibrator and target workflows.
 
     Args:
@@ -25,9 +25,21 @@ def make_input_json(mspath, calsols=''):
             ms = files[-1]
             out.write(f'   {{"class": "Directory", "path":"{ms}"}}\n')
         out.write('  ],\n')
-        if calsols:
-                calsolpath = os.path.abspath(calsols)
-                out.write(f' "cal_solutions": {{"class": "File", "path":"{calsolpath}"}}\n')
+        if calsols and targetsols:
+            calsolpath = os.path.abspath(calsols)
+            out.write(f' "cal_solutions": {{"class": "File", "path":"{calsolpath}"}},\n')
+        else:
+            calsolpath = os.path.abspath(calsols)
+            out.write(f' "cal_solutions": {{"class": "File", "path":"{calsolpath}"}}\n')
+        if targetsols and not calsols:
+            print('Only target solutions given, preparing JSON file for lofar-vlbi pipeline.')
+            calsolpath = os.path.abspath(targetsols)
+            out.write(f' "solset": {{"class": "File", "path":"{calsolpath}"}}\n')
+            out.write(f' "configfile": {{"class": "File", "path":"{path_facetselfcal_config}"}}\n')
+            out.write(f' "h5merger": {{"class": "Directory", "path":"{path_lofar_helpers}"}}\n')
+            out.write(f' "selfcal": {{"class": "DIrectory", "path":"{path_selfcal}"}}\n')
+
+            
         out.write('}\n')
     print('Input JSON written to mslist.json')
 
@@ -35,7 +47,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate an input file for LINC containing measurement sets and, optionally, the calibrator solutions.')
     parser.add_argument('mspath', type=str, help='Path where input measurement sets are located.')
     parser.add_argument('--calsols', type=str, default='', help='Path to the final LINC calibrator solution file (usually cal_solutions.h5).')
+    parser.add_argument('--targetsols', type=str, default='', help='Path to the final LINC target solution file (usually cal_solutions.h5).')
+    parser.add_argument('--path_facetselfcal_config', type=str, default='', help='Path to the config file for facetselfcal.py.')
+    parser.add_argument('--path_lofar_helpers', type=str, default='', help='Path to the lofar_helpers repository.')
+    parser.add_argument('--path_selfcal', type=str, default='', help='Path to facetselfcal?')
 
     args = vars(parser.parse_args())
 
-    make_input_json(args['mspath'], args['calsols'])
+    make_input_json(args['mspath'], args['calsols'], args['targetsols'], args['path_facetselfcal_config'], args['path_lofar_helpers'], args['path_selfcal'])
---
+++ runners/run_lofar-vlbi_singularity.sh
@@ -0,0 +1,110 @@
+# Update these variables to reflect your environment.
+TMPDIR=$PWD
+## WORKDIR is where all the other directories will be stored.
+WORKDIR=$(mktemp -d -p "$TMPDIR")
+{
+echo $WORKDIR
+## Location of LINC. This must be a user-writable location for this wrapper script.
+## If it does not exist, this script will attempt to clone the LINC repository to the given path.
+LINC_DATA_ROOT=$WORKDIR/LINC
+VLBI_DATA_ROOT=$WORKDIR/VLBI_cwl
+LOFAR_HELPERS_ROOT=$WORKDIR/lofar_helpers
+FACETSELFCAL_ROOT=$WORKDIR/lofar_facet_selfcal
+
+
+#DATADIR=/data2/sweijen/Quasar_Anniek/P240+30/testdata/
+DATADIR=/data2/sweijen/Quasar_Anniek/P240+30/
+TARGETSOLS=/data2/sweijen/Quasar_Anniek/rundir_P240_30/tmp.uCPXXKlDci/results_LINC_calibrator/cal_solutions.h5
+
+# Add any directory that should be accessible during the run here as a comma-separated list.
+BINDPATHS=/data1,/data2,$PWD
+
+## Final results will be copied here.
+RESULTSDIR=$WORKDIR/results_VLBI_CWL
+## Logs of the various steps will be put here.
+LOGSDIR=$WORKDIR/logs_VLBI_CWL
+## Temporary files are stored here.
+## The trailing slash is important here.
+TMPDIR=$WORKDIR/tmpdir_VLBI_CWL/
+
+SIMG=/net/achterrijn/data1/sweijen/software/containers/lofar_sksp_v4.0.2_x86-64_cascadelake_cascadelake_avx512_mkl_cuda_ddf.sif
+
+# Update these variables to tune performance.
+## Limit the number of DP3 processes running simultaneously to this amount.
+MAX_DP3_PROCS=8
+
+CORES_MACHINE=`nproc`
+CORES_CALIBCAL=$(($CORES_MACHINE/$MAX_DP3_PROCS))
+CORES_PREDICT=$(($CORES_MACHINE/$MAX_DP3_PROCS))
+
+#
+# Do not update below this line
+#
+export LINC_DATA_ROOT
+export VLBI_DATA_ROOT
+git clone https://git.astron.nl/RD/VLBI-cwl.git $VLBI_DATA_ROOT
+git clone https://github.com/jurjen93/lofar_helpers.git $LOFAR_HELPERS_ROOT
+git clone https://github.com/rvweeren/lofar_facet_selfcal.git $FACETSELFCAL_ROOT
+
+# Check if LINC directory exists or is valid.
+if [ ! -d $LINC_DATA_ROOT ]; then
+    echo $LINC_DATA_ROOT does not exist and will be created. Cloning LINC...
+    mkdir -p $LINC_DATA_ROOT
+    git clone https://git.astron.nl/RD/LINC.git $LINC_DATA_ROOT
+    #cd $LINC_DATA_ROOT && git checkout d4741f0
+    #cd $WORKDIR
+fi
+# If the directory exists, check if it is empty.
+if [ -d $LINC_DATA_ROOT ] && [ ! -d $LINC_DATA_ROOT/steps ]; then
+    echo $LINC_DATA_ROOT exists, but is empty. Cloning LINC...
+    git clone https://git.astron.nl/RD/LINC.git $LINC_DATA_ROOT
+    #cd $LINC_DATA_ROOT && git checkout d4741f0
+    #cd $WORKDIR
+fi
+# If the directory is not empty, check if it contains LINC
+if [ -d $LINC_DATA_ROOT ] && [ ! -d $LINC_DATA_ROOT/steps ]; then
+    echo WARNING: $LINC_DATA_ROOT is not empty, but required LINC folders are not found.
+    exit 1
+elif [ -d $LINC_DATA_ROOT ] && [ -d $LINC_DATA_ROOT/steps ]; then
+    echo $LINC_DATA_ROOT exists and seems to contain LINC. Continueing...
+fi
+
+# Prepare workflow files.
+#echo Overriding workflow core requirements with user settings.
+sed -i "s/PREFACTOR_DATA_ROOT/LINC_DATA_ROOT/" $VLBI_DATA_ROOT/steps/*.cwl
+
+echo "Overriding rfistrategies with Lua >5.3 compatible ones from AOFlagger repository"
+wget https://gitlab.com/aroffringa/aoflagger/-/raw/master/data/strategies/lofar-default.lua -O $LINC_DATA_ROOT/rfistrategies/lofar-default.lua
+
+echo Making sure all scripts are executable
+chmod 755 $LINC_DATA_ROOT/scripts/*.py
+
+echo Making sure all shebang lines use /usr/bin/env python instead of /usr/bin/env
+for f in $LINC_DATA_ROOT/scripts/*.py; do
+    sed -i "s?\#\!/usr/bin/python?\#\!/usr/bin/env python?" $f
+done
+
+# Pass along necessary variables to the container.
+export APPTAINERENV_LINC_DATA_ROOT=$LINC_DATA_ROOT
+export APPTAINERENV_VLBI_DATA_ROOT=$VLBI_DATA_ROOT
+export APPTAINERENV_RESULTSDIR=$WORKDIR/results_VLBI_CWL/
+export APPTAINERENV_LOGSDIR=$WORKDIR/logs_VLBI_CWL/
+export APPTAINERENV_TMPDIR=$WORKDIR/tmpdir_VLBI_CWL/
+export APPTAINERENV_PREPEND_PATH=$LINC_DATA_ROOT/scripts:$VLBI_DATA_ROOT/scripts
+
+mkdir -p $RESULTSDIR
+mkdir -p $LOGSDIR
+mkdir -p $TMPDIR
+cd $WORKDIR
+
+wget https://raw.githubusercontent.com/tikk3r/lofar-grid-hpccloud/fedora-py3/runners/create_ms_list.py
+singularity exec -B $PWD,$BINDPATHS $SIMG python create_ms_list.py $DATADIR --targetsols=$TARGETSOLS --path_facetselfcal_config=/dev/null --path_lofar_helpers=$LOFAR_HELPERS_ROOT --path_selfcal=$FACETSELFCAL_ROOT
+
+echo VLBI-cwl starting
+# Switch to a non-GUI backend to avoid plotting issues.
+echo export MPLBACKEND='Agg' > tmprunner.sh
+echo export PYTHONPATH=\$VLBI_DATA_ROOT/scripts:\$LINC_DATA_ROOT/scripts:\$PYTHONPATH >> tmprunner.sh
+echo 'cwltool --parallel --preserve-entire-environment --no-container --tmpdir-prefix=$TMPDIR --outdir=$RESULTSDIR --log-dir=$LOGSDIR $VLBI_DATA_ROOT/workflows/delay-calibration.cwl mslist.json 2>&1' >> tmprunner.sh
+time singularity exec -B $PWD,$BINDPATHS $SIMG bash tmprunner.sh 2>&1
+echo VLBI-cwl ended
+} | tee $WORKDIR/job_output_test.txt 2>&1
