180941957399027cfea8b28e074106f96fe30986
Frits Sweijen <tikk3r@noreply.github.com>
2023-08-15 11:54:45 +0200

    Update VLBI runner

--- runners/run_lofar-vlbi_singularity.sh
+++ runners/run_lofar-vlbi_singularity.sh
@@ -98,7 +98,7 @@ mkdir -p $TMPDIR
 cd $WORKDIR
 
 wget https://raw.githubusercontent.com/tikk3r/lofar-grid-hpccloud/fedora-py3/runners/create_ms_list.py
-singularity exec -B $PWD,$BINDPATHS $SIMG python create_ms_list.py $DATADIR --vlbi --solset=$TARGETSOLS --configfile=$VLBI_DATA_ROOT/facetselfcal_config.txt --h5merger=$LOFAR_HELPERS_ROOT --selfcal=$FACETSELFCAL_ROOT
+singularity exec -B $PWD,$BINDPATHS $SIMG python create_ms_list.py $DATADIR --vlbi --solset=$TARGETSOLS --configfile=$VLBI_DATA_ROOT/facetselfcal_config.txt --h5merger=$LOFAR_HELPERS_ROOT --facetselfcal=$FACETSELFCAL_ROOT
 
 echo VLBI-cwl starting
 # Switch to a non-GUI backend to avoid plotting issues.
