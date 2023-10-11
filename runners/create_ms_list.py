977ecde42d585442b617dd0e8d0e239606e9d5bc
Frits Sweijen <tikk3r@noreply.github.com>
2023-10-11 16:15:41 +0200

    Add argument for delay solutions

--- runners/create_ms_list.py
+++ runners/create_ms_list.py
@@ -191,6 +191,7 @@ if __name__ == '__main__':
     vlbiparser.add_argument('--reference_stationSB', type=int, default=104, help='Name of the soltab with LINC target phase solutions.')
     vlbiparser.add_argument('--number_cores', type=int, default=12, help='Number of cores to use per job for tasks with high I/O or memory.')
     vlbiparser.add_argument('--image_catalogue', type=cwl_file, default='lotss_catalogue.csv', help='Catalogue containing target sources for imaging after delay calibration.')
+    vlbiparser.add_argument('--delay_solset', type=cwl_file, default='' , help='Delay calibration solutions.')
 
     dparser = parser.add_argument_group('== Data and calibration ==')
     dparser.add_argument('--cal_solutions', type=cwl_file, default='', help='Path to the final LINC calibrator solution file.')
