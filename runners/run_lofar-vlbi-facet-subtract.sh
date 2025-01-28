151c667463bd003a5c0749f500a7495adeb6705c
Frits Sweijen <tikk3r@users.noreply.github.com>
2025-01-28 20:57:32 +0000

    Add m and c options to runner

--- runners/run_lofar-vlbi-facet-subtract.sh
+++ runners/run_lofar-vlbi-facet-subtract.sh
@@ -14,7 +14,7 @@ if [[ $1 == "-h" || $1 == "--help" ]]; then
     exit 0
 fi
 
-while getopts ":d:s:r:l:f:b:e:" opt; do
+while getopts ":d:s:r:l:f:b:e:m:c:" opt; do
     case $opt in
         d) DATADIR="$OPTARG"
         ;;
