e1317125aeee66717a7b57a858d2f8fa9a5da5ee
Frits Sweijen <tikk3r@users.noreply.github.com>
2025-06-02 22:21:48 +0100

    Add main executable

--- flocs_runners/flocs_run.py
+++ flocs_runners/flocs_run.py
@@ -1,50 +1,11 @@
 #!/usr/bin/env python
 import json
 import os
-from dataclasses import dataclass
-from typing import Any, Optional
+from typing import Optional
 
 import typer
 
 from . import linc_runner, vlbi_runner
-
-
-@dataclass
-class cwl_file:
-    value: Any
-
-    def __init__(self, entry: Optional[str]):
-        """Create a CWL-friendly file entry."""
-        if entry is None:
-            self.value = None
-        if entry.lower() == "null":
-            self.value = None
-        else:
-            self.value = json.loads(
-                f'{{"class": "File", "path":"{os.path.abspath(entry)}"}}'
-            )
-
-
-# def cwl_file(entry: str) -> Optional[str]:
-#    """Create a CWL-friendly file entry."""
-#    if entry is None:
-#        return None
-#    if entry.lower() == "null":
-#        return None
-#    else:
-#        return json.loads(f'{{"class": "File", "path":"{os.path.abspath(entry)}"}}')
-#
-#
-# def cwl_dir(entry: str) -> Optional[str]:
-#    """Create a CWL-friendly directory entry."""
-#    if entry is None:
-#        return None
-#    if entry.lower() == "null":
-#        return None
-#    else:
-#        return json.loads(
-#            f'{{"class": "Directory", "path":"{os.path.abspath(entry)}"}}'
-#        )
 
 
 def main():
