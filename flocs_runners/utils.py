b696587a7909332e7cc188b7771351308dbd4447
Frits Sweijen <tikk3r@users.noreply.github.com>
2025-06-02 22:21:38 +0100

    Add utility module

--- flocs_runners/utils.py
+++ flocs_runners/utils.py
@@ -1,22 +1,23 @@
 from typing import Optional
 import json
+import os
 
 
 def cwl_file(entry: str) -> Optional[str]:
-   """Create a CWL-friendly file entry."""
-   if entry is None:
-       return None
-   if entry.lower() == "null":
-       return None
-   else:
-       return json.loads(f'{{"class": "File", "path":"{os.path.abspath(entry)}"}}')
+    """Create a CWL-friendly file entry."""
+    if entry is None:
+        return None
+    if entry.lower() == "null":
+        return None
+    else:
+        return json.loads(f'{{"class": "File", "path":"{os.path.abspath(entry)}"}}')
+
 
 def cwl_dir(entry: str) -> Optional[str]:
-   """Create a CWL-friendly file entry."""
-   if entry is None:
-       return None
-   if entry.lower() == "null":
-       return None
-   else:
-       return json.loads(f'{{"class": "File", "path":"{os.path.abspath(entry)}"}}')
-
+    """Create a CWL-friendly file entry."""
+    if entry is None:
+        return None
+    if entry.lower() == "null":
+        return None
+    else:
+        return json.loads(f'{{"class": "File", "path":"{os.path.abspath(entry)}"}}')
