289e790b4e55af97d6896be6b26d34689ec5f492
Frits Sweijen <tikk3r@users.noreply.github.com>
2025-08-20 13:29:08 +0100

    Code reorganisation

--- flocs_runners/utils.py
+++ flocs_runners/utils.py
@@ -1,10 +1,6 @@
 import glob
 import json
 import os
-import subprocess
-import sys
-import tempfile
-from enum import Enum
 from typing import Optional, Union
 
 import casacore.tables as ct
@@ -129,293 +125,60 @@ def get_reffreq(msfile: str) -> float:
     return freq
 
 
-class LINCJSONConfig:
-    """Class for generating JSON configuration files to be passed to the LINC pipeline."""
+def setup_toil_slurm(slurm_params: dict):
+    """Sets the TOIL_SLURM_ARGS environment variable with information for the Slurm scheduler.
 
-    class OBS_TYPE(Enum):
-        CALIBRATOR = 1
-        TARGET = 2
+    It will always set to export all variables and adds SLURM details such as accounts and partitions if specified.
 
-    def __init__(
-        self,
-        mspath: str,
-        ms_suffix: str = ".MS",
-        prefac_h5parm={"path": ""},
-        update_version_file: bool = False,
+    Args:
+        slurm_params (dict[str]): dictionary with slurm options. Accepted keys are `account`, `queue` and `time`.
+    """
+    os.environ["TOIL_SLURM_ARGS"] = "--export=ALL "
+    if "queue" in slurm_params:
+        os.environ["TOIL_SLURM_ARGS"] += f"-p {slurm_params["queue"]} "
+    if "account" in slurm_params:
+        os.environ["TOIL_SLURM_ARGS"] += f"-A {slurm_params["account"]} "
+    if "time" in slurm_params:
+        os.environ["TOIL_SLURM_ARGS"] += f"-t {slurm_params["time"]} "
+
+
+def verify_slurm_environment_toil():
+    failed = False
+    if "CWL_SINGULARITY_CACHE" not in os.environ:
+        logger.critical(
+            "CWL_SINGULARITY_CACHE not found in the environment. Ensure it is set to where you have stored `astronrd_linc_latest.sif`."
+        )
+        failed = True
+    elif not os.path.isfile(
+        os.path.join(os.environ["CWL_SINGULARITY_CACHE"], "astronrd_linc_latest.sif")
     ):
-        self.configdict = {}
-
-        filedir = os.path.join(mspath, f"*{ms_suffix}")
-        logger.info(f"Searching {filedir}")
-        files = sorted(glob.glob(filedir))
-        logger.info(f"Found {len(files)} files")
-
-        if not prefac_h5parm["path"].endswith("h5") and not prefac_h5parm[
-            "path"
-        ].endswith("h5parm"):
-            mslist = []
-            for ms in files:
-                x = json.loads(f'{{"class": "Directory", "path":"{ms}"}}')
-                mslist.append(x)
-            self.configdict["msin"] = mslist
-        else:
-            prefac_freqs = get_prefactor_freqs(
-                solname=prefac_h5parm["path"], solset="calibrator"
-            )
-
-            mslist = []
-            for dd in files:
-                if check_dd_freq(dd, prefac_freqs):
-                    mslist.append(dd)
-
-            final_mslist = []
-            for ms in mslist:
-                x = json.loads(f'{{"class": "Directory", "path":"{ms}"}}')
-                final_mslist.append(x)
-            self.configdict["msin"] = final_mslist
-        self.create_linc_versions_file(update_version_file)
-
-    def add_entry(self, key: str, value: object):
-        if "ATeam" in key:
-            self.configdict["A-Team_skymodel"] = value
-        else:
-            self.configdict[key] = value
-
-    def create_linc_versions_file(self, overwrite=False):
-        if "LINC_DATA_ROOT" not in os.environ:
-            raise ValueError(
-                "WARNING: LINC_DATA_ROOT environment variable has not been set. Cannot generate $LINC_DATA_ROOT/.versions file."
-            )
-        linc_version = subprocess.check_output(
-            f"cd {os.environ["LINC_DATA_ROOT"]} && git describe --tags",
-            shell=True,
-            text=True,
+        raise FileNotFoundError(
+            "Cannot find astornrd_linc_latest.sif in CWL_SINGULARITY_CACHE."
         )
-        pip_versions = subprocess.check_output(
-            "pip freeze | sed 's/==/: /g'", shell=True
+    if "APPTAINERENV_PULLDIR" not in os.environ:
+        logger.critical(
+            "APPTAINERENV_PULLDIR not found in the environment. Ensure it is set to where you have stored `astronrd_linc_latest.sif`."
         )
-        linc_version_file = os.path.join(os.environ["LINC_DATA_ROOT"], ".versions")
-
-        if os.path.isfile(linc_version_file) and not overwrite:
-            logger.info(f"Using existing {os.environ['LINC_DATA_ROOT']}/.versions")
-        if not os.path.isfile(linc_version_file) or overwrite:
-            with open(linc_version_file, "wb") as f:
-                f.write(f"LINC: {linc_version}".encode("utf-8"))
-                f.write(pip_versions)
-
-    def save(self, fname: str):
-        if not fname.endswith(".json"):
-            fname += ".json"
-        with open(fname, "w") as outfile:
-            json.dump(self.configdict, outfile, indent=4)
-        logger.info(f"Written configuration to {fname}")
-        self.configfile = fname
-
-    def setup_rundir(self, workdir):
-        if "calibrator" in self.configfile:
-            self.rundir = tempfile.mkdtemp(prefix="tmp.LINC_calibrator.", dir=workdir)
-        elif "target" in self.configfile:
-            self.rundir = tempfile.mkdtemp(prefix="tmp.LINC_target.", dir=workdir)
-        else:
-            logger.warning("Unknown config file passed; exiting.")
-            sys.exit(-1)
-
-    def run_workflow(
-        self,
-        runner: str = "toil",
-        scheduler: str = "slurm",
-        workdir: str = os.getcwd(),
-        container: str = "",
-        slurm_params: dict = {},
+        failed = True
+    elif not os.path.isfile(
+        os.path.join(os.environ["APPTAINERENV_PULLDIR"], "astronrd_linc_latest.sif")
     ):
-        if self.configfile is None:
-            raise RuntimeError("No config file has been created. Save it first.")
-        elif "calibrator" in self.configfile:
-            self.mode = self.OBS_TYPE.CALIBRATOR
-        elif "target" in self.configfile:
-            self.mode = self.OBS_TYPE.TARGET
-        elif ("calibrator" not in self.configfile) and (
-            "target" not in self.configfile
-        ):
-            raise RuntimeError(
-                "Cannot deduce LINC workflow to run from config file name. Ensure either `calibrator` or `target` is present in the file name."
-            )
-        else:
-            raise RuntimeError("Something unexpected went wrong with the config file.")
-        self.setup_rundir(workdir)
-        self.setup_apptainer_variables(self.rundir)
-        logger.info(
-            f"Running workflow with {runner} under {scheduler} in {self.rundir}"
+        raise FileNotFoundError(
+            "Cannot find astornrd_linc_latest.sif in APPTAINERENV_PULLDIR."
         )
-
-        if runner == "cwltool":
-            cmd = (
-                "cwltool "
-                + "--parallel "
-                + "--preserve-entire-environment "
-                + "--no-container "
-                + f"--tmpdir-prefix={os.environ['APPTAINERENV_TMPDIR']} "
-                + f"--outdir={os.environ['APPTAINERENV_RESULTSDIR']} "
-                + f"--log-dir={os.environ['APPTAINERENV_LOGSDIR']} "
-            )
-            if self.mode is self.OBS_TYPE.CALIBRATOR:
-                cmd += f"{os.environ['LINC_DATA_ROOT']}/workflows/HBA_calibrator.cwl "
-            elif self.mode is self.OBS_TYPE.TARGET:
-                cmd += f"{os.environ['LINC_DATA_ROOT']}/workflows/HBA_target.cwl "
-            cmd += f"{self.configfile}"
-
-            if scheduler == "slurm":
-                if container:
-                    cmd = add_apptainer_skeleton(contents=cmd, container=container)
-                wrapped_cmd = add_slurm_skeleton(
-                    contents=cmd,
-                    time="24:00:00",
-                    cores=32,
-                    job_name="LINC_Calibrator",
-                    **slurm_params,
-                )
-                with open("temp_jobscript.sh", "w") as f:
-                    f.write(wrapped_cmd)
-                logger.info("Written temporary jobscript to temp_jobscript.sh")
-                out = subprocess.check_output(["sbatch", "temp_jobscript.sh"]).decode(
-                    "utf-8"
-                )
-            elif scheduler == "singleMachine":
-                if container:
-                    cmd = add_apptainer_skeleton(contents=cmd, container=container)
-                logger.info(f"Running command:\n{cmd}")
-                out = subprocess.check_output(cmd.split(" ")).decode("utf-8")
-                print(out)
-        elif runner == "toil":
-            self.verify_slurm_environment_toil()
-            dir_coordination, dir_slurmlogs = self.setup_toil_directories(self.rundir)
-            self.setup_toil_slurm(slurm_params)
-            cmd = ["toil-cwl-runner"]
-            if scheduler == "slurm":
-                cmd += ["--batchSystem", "slurm"]
-            elif scheduler == "singleMachine":
-                cmd += ["--batchSystem", "singleMachine"]
-            else:
-                raise ValueError(f"Unsupported scheduler `{scheduler}` provided.")
-            cmd += ["--no-read-only"]
-            cmd += ["--retryCount", "3"]
-            cmd += ["--singularity"]
-            cmd += ["--disableCaching"]
-            cmd += ["--writeLogsFromAllJobs", "True"]
-            cmd += ["--logFile", "full_log.log"]
-            cmd += ["--writeLogs", os.environ["APPTAINERENV_LOGSDIR"]]
-            cmd += ["--outdir", os.environ["APPTAINERENV_RESULTSDIR"]]
-            cmd += ["--tmp-outdir-prefix", os.environ["APPTAINERENV_TMPDIR"]]
-            cmd += ["--jobStore", os.path.join(self.rundir, "jobstore")]
-            cmd += ["--workDir", workdir]
-            cmd += ["--coordinationDir", dir_coordination]
-            cmd += ["--tmpdir-prefix", os.environ["APPTAINERENV_TMPDIR"]]
-            cmd += ["--disableAutoDeployment", "True"]
-            cmd += ["--bypass-file-store"]
-            cmd += [
-                "--batchLogsDir",
-                os.path.join(os.environ["APPTAINERENV_LOGSDIR"], dir_slurmlogs),
-            ]
-            cmd += ["--no-compute-checksum"]
-            if self.mode is self.OBS_TYPE.CALIBRATOR:
-                cmd += [
-                    os.path.join(
-                        os.environ["LINC_DATA_ROOT"], "workflows", "HBA_calibrator.cwl"
-                    )
-                ]
-            elif self.mode is self.OBS_TYPE.TARGET:
-                cmd += [
-                    os.path.join(
-                        os.environ["LINC_DATA_ROOT"], "workflows", "HBA_target.cwl"
-                    )
-                ]
-            cmd += [self.configfile]
-            out = subprocess.check_output(cmd)
-
-    def setup_apptainer_variables(self, workdir):
-        out = (
-            subprocess.check_output(["singularity", "--version"])
-            .decode("utf-8")
-            .strip()
+    if "APPTAINERENV_CACHEDIR" not in os.environ:
+        logger.critical(
+            "APPTAINERENV_CACHEDIR not found in the environment. Ensure it is set to where you have stored `astronrd_linc_latest.sif`."
         )
-        if "apptainer" in out:
-            os.environ["APPTAINERENV_LINC_DATA_ROOT"] = os.environ["LINC_DATA_ROOT"]
-            os.environ["APPTAINERENV_RESULTSDIR"] = (
-                f"{workdir}/results_LINC_calibrator/"
-            )
-            os.environ["APPTAINERENV_LOGSDIR"] = f"{workdir}/logs_LINC_calibrator/"
-            os.environ["APPTAINERENV_TMPDIR"] = f"{workdir}/tmpdir_LINC_calibrator/"
-            os.environ["APPTAINERENV_PREPEND_PATH"] = (
-                f"{os.environ['LINC_DATA_ROOT']}/scripts"
-            )
-            os.mkdir(os.environ["APPTAINERENV_LOGSDIR"])
-            os.mkdir(os.environ["APPTAINERENV_TMPDIR"])
-            os.mkdir(os.environ["APPTAINERENV_RESULTSDIR"])
-        elif "singularity" in out:
-            os.environ["SINGULARITYENV_LINC_DATA_ROOT"] = os.environ["LINC_DATA_ROOT"]
-            os.environ["SINGULARITYENV_RESULTSDIR"] = (
-                f"{workdir}/results_LINC_calibrator/"
-            )
-            os.environ["SINGULARITYENV_LOGSDIR"] = f"{workdir}/logs_LINC_calibrator/"
-            os.environ["SINGULARITYENV_TMPDIR"] = f"{workdir}/tmpdir_LINC_calibrator/"
-            os.environ["SINGULARITYENV_PREPEND_PATH"] = (
-                f"{os.environ['LINC_DATA_ROOT']}/scripts"
-            )
-            os.mkdir(os.environ["SINGULARITYENV_LOGSDIR"])
-            os.mkdir(os.environ["SINGULARITYENV_TMPDIR"])
-            os.mkdir(os.environ["SINGULARITYENV_RESULTSDIR"])
-        os.environ["PYTHONPATH"] = "$LINC_DATA_ROOT/scripts:" + os.environ["PYTHONPATH"]
-
-    def setup_toil_directories(self, workdir: str) -> tuple[str, str]:
-        dir_coordination = os.path.join(workdir, "coordination")
-        try:
-            os.mkdir(dir_coordination)
-        except FileExistsError:
-            print("Coordination directory already exists, not overwriting.")
-
-        dir_slurmlogs = os.path.join(os.environ["APPTAINERENV_LOGSDIR"], "slurmlogs")
-        try:
-            os.mkdir(dir_slurmlogs)
-        except FileExistsError:
-            print("Slurm log directory already exists, not overwriting.")
-
-        return (dir_coordination, dir_slurmlogs)
-
-    def setup_toil_slurm(self, slurm_params: dict):
-        """Sets the TOIL_SLURM_ARGS environment variable with information for the Slurm scheduler.
-
-        It will always set to export all variables and adds SLURM details such as accounts and partitions if specified.
-
-        Args:
-            slurm_params (dict[str]): dictionary with slurm options. Accepted keys are `account`, `queue` and `time`.
-        """
-        os.environ["TOIL_SLURM_ARGS"] = "--export=ALL "
-        if "queue" in slurm_params:
-            os.environ["TOIL_SLURM_ARGS"] += f"-p {slurm_params["queue"]} "
-        if "account" in slurm_params:
-            os.environ["TOIL_SLURM_ARGS"] += f"-A {slurm_params["account"]} "
-        if "time" in slurm_params:
-            os.environ["TOIL_SLURM_ARGS"] += f"-t {slurm_params["time"]} "
-
-    def verify_slurm_environment_toil(self):
-        failed = False
-        if "CWL_SINGULARITY_CACHE" not in os.environ:
-            logger.critical("CWL_SINGULARITY_CACHE not found in the environment. Ensure it is set to where you have stored `astronrd_linc_latest.sif`.")
-            failed = True
-        elif not os.path.isfile(os.path.join(os.environ["CWL_SINGULARITY_CACHE"], "astronrd_linc_latest.sif")):
-            raise FileNotFoundError("Cannot find astornrd_linc_latest.sif in CWL_SINGULARITY_CACHE.")
-        if "APPTAINERENV_PULLDIR" not in os.environ:
-            logger.critical("APPTAINERENV_PULLDIR not found in the environment. Ensure it is set to where you have stored `astronrd_linc_latest.sif`.")
-            failed = True
-        elif not os.path.isfile(os.path.join(os.environ["APPTAINERENV_PULLDIR"], "astronrd_linc_latest.sif")):
-            raise FileNotFoundError("Cannot find astornrd_linc_latest.sif in APPTAINERENV_PULLDIR.")
-        if "APPTAINERENV_CACHEDIR" not in os.environ:
-            logger.critical("APPTAINERENV_CACHEDIR not found in the environment. Ensure it is set to where you have stored `astronrd_linc_latest.sif`.")
-            failed = True
-        elif not os.path.isfile(os.path.join(os.environ["APPTAINERENV_CACHEDIR"], "astronrd_linc_latest.sif")):
-            raise FileNotFoundError("Cannot find astornrd_linc_latest.sif in APPTAINERENV_CACHEDIR.")
-        if failed:
-            raise RuntimeError("One or more critical environment variables were not set.")
+        failed = True
+    elif not os.path.isfile(
+        os.path.join(os.environ["APPTAINERENV_CACHEDIR"], "astronrd_linc_latest.sif")
+    ):
+        raise FileNotFoundError(
+            "Cannot find astornrd_linc_latest.sif in APPTAINERENV_CACHEDIR."
+        )
+    if failed:
+        raise RuntimeError("One or more critical environment variables were not set.")
 
 
 def add_slurm_skeleton(
