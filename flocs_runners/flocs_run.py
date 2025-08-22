f2775f6f28a35887ef968dd6c852df0fca44cc7c
Frits Sweijen <tikk3r@users.noreply.github.com>
2025-08-22 17:27:44 +0100

    Move LINC_DATA_ROOT warning to LINC instantiation
    Exit

--- flocs_runners/flocs_run.py
+++ flocs_runners/flocs_run.py
@@ -9,10 +9,6 @@ from . import linc_runner, vlbi_runner
 
 
 def main():
-    if "LINC_DATA_ROOT" not in os.environ:
-        raise ValueError(
-            "WARNING: LINC_DATA_ROOT environment variable has not been set. Cannot generate $LINC_DATA_ROOT/.versions file."
-        )
     app = typer.Typer(add_completion=False)
     app.add_typer(linc_runner.app, name="linc")
     app.add_typer(vlbi_runner.app, name="vlbi")
