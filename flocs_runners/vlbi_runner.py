09ed31edaab9290416812bd8d84ee6c3dde62234
Frits Sweijen <tikk3r@users.noreply.github.com>
2025-08-27 16:57:49 +0200

    Include obsid in VLBI directory

--- flocs_runners/vlbi_runner.py
+++ flocs_runners/vlbi_runner.py
@@ -1,9 +1,10 @@
 from .utils import (
-    cwl_file,
-    cwl_dir,
     add_apptainer_skeleton,
     add_slurm_skeleton,
     check_dd_freq,
+    cwl_file,
+    cwl_dir,
+    extract_obsid_from_ms,
     get_prefactor_freqs,
     setup_toil_slurm,
     verify_slurm_environment_toil,
@@ -77,6 +78,7 @@ class VLBIJSONConfig:
                 x = json.loads(f'{{"class": "Directory", "path":"{ms}"}}')
                 final_mslist.append(x)
             self.configdict["msin"] = final_mslist
+        self.obsid = extract_obsid_from_ms(self.configdict["msin"][0]["path"])
         self.create_linc_versions_file(update_version_file)
 
     def add_entry(self, key: str, value: object):
@@ -160,7 +162,7 @@ class VLBIJSONConfig:
 
     def move_results_from_rundir(self):
         date = strftime("%Y_%m_%d-%H_%M_%S", gmtime())
-        subprocess.check_output(["mv", self.rundir, f"LINC_{self.mode}_{date}"])
+        subprocess.check_output(["mv", self.rundir, f"LOFAR-VLBI_{self.mode}_{self.obsid}_{date}"])
 
     def run_workflow(
         self,
