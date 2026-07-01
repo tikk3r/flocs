a4dc1ad1041f04414bfd2d753c94e02cee66b8c8
Frits Sweijen <tikk3r@users.noreply.github.com>
2026-07-01 13:17:27 +0100

    Bump wsclean

--- build_environment_variables.sh
+++ build_environment_variables.sh
@@ -10,7 +10,7 @@ export LOSOTO_VERSION=2.7.1
 export OPENBLAS_VERSION=v0.3.31
 export PYBDSF_VERSION=8b33037
 export PYTHON_CASACORE_VERSION=3.6.1
-export WSCLEAN_VERSION=8e035b30
+export WSCLEAN_VERSION=2fe6c944
 
 # Expert settings below. Generally these won't have to be touched.
 # General environment settings.
